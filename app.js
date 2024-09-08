cfa708dfdc5cf0e4117cea184cee737d90eece03
Steven Nielsen <steven.nielsen226@gmail.com>
2024-09-07 20:34:38 -0700

    redid complete template before uploading to render.com

--- app.js
+++ app.js
@@ -2,39 +2,37 @@ const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
 const app = express();
-const books = require("./book-data").books;
-// Set the view engine to pug
+let books = require("./book-data").books;
+
+//Set the view engine to pug
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-// Body parsing middleware
+
+//Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Allow us to do our puts and deletes
+// Allows us to do our puts and deletes
 app.use(methodOverride("_method"));
 // Define routes here
 app.get("/", function (req, res) {
   res.render("books", { books });
 });
+
 app.post("/", function (req, res) {
   books.push(req.body);
   res.redirect("/");
 });
+
+app.delete("/:id", function (req, res) {
+  const filterdList = books.filter((book) => book.isbn !== req.params.id);
+  books = filterdList;
+  res.redirect("/");
+});
+
 app.get("/new", function (req, res) {
   res.render("new-book");
 });
-app.get("/:id", function (req, res) {
-  const isbn = req.params.id;
-  const book = books.find((book) => book.isbn === isbn);
-  res.render("book-details", { book });
-});
-app.get("/new", function (req, res) {
-  res.render("new-book");
-});
-app.get("edit/:id", function (req, res) {
-  const isbn = req.params.id;
-  const book = books.find((book) => book.isbn === isbn);
-  res.render("edit-book", { book });
-});
+
 app.get("/:id", function (req, res) {
   const isbn = req.params.id;
   const book = books.find((book) => book.isbn === isbn);
@@ -44,10 +42,15 @@ app.get("/:id", function (req, res) {
 app.put("/:id", function (req, res) {
   const isbn = req.params.id;
   const bookIndex = books.findIndex((book) => book.isbn === isbn);
-  if (book) {
-    books[bookIndex] = req.body;
-    res.redirect("/");
-  }
+  books[bookIndex] = req.body;
+  console.log(books);
+  res.redirect("/");
+});
+
+app.get("/edit/:id", function (req, res) {
+  const isbn = req.params.id;
+  const book = books.find((book) => book.isbn === isbn);
+  res.render("edit-book", { book });
 });
 // Start the server
 app.listen(3000, () => console.log("Server running on port 3000"));
